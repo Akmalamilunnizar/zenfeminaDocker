eaac4b3026e9e0c2dbe0bfc2799a577424717d1c
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-05-02 12:12:07 +0700

    style: add swal js

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -40,6 +40,7 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/sweetalert2.all.min.js"></script>
     <script src="{{ asset('js/utils.js') }}"></script>
     @apexchartsScripts
 
