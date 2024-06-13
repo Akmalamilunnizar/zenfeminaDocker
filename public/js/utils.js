080e4b82d7c1d395196ba9417f0de2f9a21aaaac
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-06-13 23:38:38 +0700

    style: show and hide password

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,15 +2,18 @@ function displayFormErrors(errors) {
     for(const [key, value] of Object.entries(errors)) {
         const input = $(`#${key}`);
 
-        if(input.attr('type') === 'file')
-        {
+        if (input.attr('type') === 'file') {
             const parent = input.parent();
             parent.addClass('is-invalid');
             parent.next().html(value[0]);
+        } else if (input.attr('type') === 'password') {
+            input.addClass('is-invalid');
+            input.nextAll('div').first().html(value[0]);
         } else {
             input.addClass('is-invalid');
             input.next().html(value[0]);
         }
+
     }
 }
 
--- resources/views/components/user_modal.blade.php
+++ resources/views/components/user_modal.blade.php
@@ -5,22 +5,22 @@
         }
 
         #togglePassword {
-            position: absolute;
-            top: 48%;
-            right: 25px;
-            transform: translateY(-50%);
+            /*position: absolute;*/
+            /*top: 48%;*/
+            /*right: 25px;*/
+            /*transform: translateY(-50%);*/
             cursor: pointer;
-            z-index: 1; /* Ensure the icon appears above the input */
+            /*z-index: 1; !* Ensure the icon appears above the input *!*/
         }
 
-        .invalid-feedback {
-    position: absolute;
-    bottom: -20px;
-    left: 0;
-    right: 0;
-    font-size: 12px;
-    color: red;
-}
+/*        .invalid-feedback {*/
+/*    position: absolute;*/
+/*    bottom: -20px;*/
+/*    left: 0;*/
+/*    right: 0;*/
+/*    font-size: 12px;*/
+/*    color: red;*/
+/*}*/
 
 
     </style>
@@ -47,11 +47,14 @@
                     </div>
                     <div class="mb-4">
                         <label for="password" class="form-label">Password</label>
-                        <div class="password-input-container">
+                        <div class="password-input-container input-group">
                             <input type="password" name="password" class="form-control" id="password">
+                            <span class="input-group-text" id="toggleContainer"><i id="togglePassword" class="toggle-password bi bi-eye-fill"></i></span>
                             <div class="invalid-feedback"></div>
-                            <span id="togglePassword" class="toggle-password bi bi-eye-fill"></span>
+
+{{--                            <span id="togglePassword" class="toggle-password bi bi-eye-fill"></span>--}}
                         </div>
+{{--                        <div class="invalid-feedback"></div>--}}
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -66,9 +69,10 @@
 @push('script')
     <script>
         const passwordInput = document.getElementById('password');
+        const toggleContainer = document.getElementById('toggleContainer');
         const toggleButton = document.getElementById('togglePassword');
 
-        toggleButton.addEventListener('click', function () {
+        toggleContainer.addEventListener('click', function () {
             if (passwordInput.type === 'password') {
                 passwordInput.type = 'text';
                 toggleButton.classList.remove('bi-eye-fill');
